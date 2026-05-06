18107007a6cbfa6a1b0e17023c3248c86ac17050
Andres <andresgomez85128@gmail.com>
2026-05-05 22:41:08 -0500

    Js

--- app/controllers/usuario_controller.py
+++ app/controllers/usuario_controller.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.services import servicios
-import threading
+import threading   
+from app import models
 
 router = APIRouter()
 
